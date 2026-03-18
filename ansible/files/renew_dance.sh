148d14fdd705a1f3a5c73072719721cb7bab1a46
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-03-18 00:37:26 -0400

    Delete ansible/files/renew_dance.sh

--- ansible/files/renew_dance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#   no need for this normally, use redirectrix2 so we don't need to stop in this case
-#service redirectrix stop &&
-#sh /root/certbot_TEMPLATE.sh &&
-#service redirectrix start
