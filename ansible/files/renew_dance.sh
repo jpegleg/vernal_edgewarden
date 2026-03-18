539ea2bd08a2d89a976514c47a890e9113489b19
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-03-18 00:05:37 -0400

    depcrecate renew_dance.sh

--- ansible/files/renew_dance.sh
+++ ansible/files/renew_dance.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-service redirectrix stop &&
-sh /root/certbot_TEMPLATE.sh &&
-service redirectrix start
+#   no need for this normally, use redirectrix2 so we don't need to stop in this case
+#service redirectrix stop &&
+#sh /root/certbot_TEMPLATE.sh &&
+#service redirectrix start
